45c8c52bdd008468870314db1e973e2e45d88796
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-09-13 10:32:58 +0100

    Corrected Api URL

--- src/FamilyHubs.ServiceDirectory.UI/Infrastructure/Configuration/ApiOptions.cs
+++ src/FamilyHubs.ServiceDirectory.UI/Infrastructure/Configuration/ApiOptions.cs
@@ -6,7 +6,7 @@ public class ApiOptions //: IApimClientConfiguration
 {
     public const string ApplicationServiceApi = "ApplicationServiceApi";
     public string ServiceDirectoryUrl { get; set; } = "https://localhost:7022/";
-    public string ReferralApiUrl { get; set; } = "http://localhost:7282";
+    public string ReferralApiUrl { get; set; } = "https://localhost:7282";
     public string SubscriptionKey { get; set; } = default!;
     public string ApiVersion { get; set; } = default!;
 }
